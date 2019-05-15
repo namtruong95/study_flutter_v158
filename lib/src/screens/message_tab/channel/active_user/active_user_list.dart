6b2e1c3bd5da88e806104fcc8352b385085b3749
Nam Truong <nam.tt@neo-lab.vn>
2019-05-15 16:12:49 +0700

    loadmore active users

--- lib/src/screens/message_tab/channel/active_user/active_user_list.dart
+++ lib/src/screens/message_tab/channel/active_user/active_user_list.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:study_flutter_v158/src/components/active_user/active_user.dart';
+import 'package:study_flutter_v158/src/shared/widgets/loading_indicator.dart';
 
 import 'active_user_item.dart';
 
@@ -10,17 +11,32 @@ class ActiveUserList extends StatefulWidget {
 }
 
 class _ActiveUserListState extends State<ActiveUserList> {
+  final _scrollController = ScrollController();
+  final _scrollThreshold = 200.0;
+
   ActiveUserBloc _activeUserBloc;
 
   @override
   void initState() {
+    this._scrollController.addListener(_onScroll);
+
     _activeUserBloc = ActiveUserBloc();
     _activeUserBloc.dispatch(FetchActiveUser());
     super.initState();
   }
 
+  void _onScroll() {
+    final maxScroll = _scrollController.position.maxScrollExtent;
+    final currentScroll = _scrollController.position.pixels;
+
+    if (maxScroll - currentScroll <= _scrollThreshold) {
+      this._activeUserBloc.dispatch(FetchActiveUser());
+    }
+  }
+
   @override
   void dispose() {
+    this._scrollController.dispose();
     _activeUserBloc.dispose();
     super.dispose();
   }
@@ -30,12 +46,26 @@ class _ActiveUserListState extends State<ActiveUserList> {
       if (state.activeUsers.isNotEmpty) {
         return ListView.builder(
           itemBuilder: (BuildContext context, int index) {
-            return UserActiveItem(
-              activeUser: state.activeUsers[index],
-            );
+            return (!state.hasReachedMax && index >= state.activeUsers.length)
+                ? Container(
+                    padding: EdgeInsets.all(10),
+                    child: Stack(
+                      children: <Widget>[
+                        CircularProgressIndicator(
+                          strokeWidth: 1,
+                        )
+                      ],
+                    ),
+                  )
+                : UserActiveItem(
+                    activeUser: state.activeUsers[index],
+                  );
           },
           scrollDirection: Axis.horizontal,
-          itemCount: state.activeUsers.length,
+          itemCount: state.hasReachedMax
+              ? state.activeUsers.length
+              : state.activeUsers.length + 1,
+          controller: this._scrollController,
         );
       }
     }
@@ -49,20 +79,30 @@ class _ActiveUserListState extends State<ActiveUserList> {
       bloc: _activeUserBloc,
       builder: (BuildContext context, ActiveUserState state) {
         return SliverFixedExtentList(
-          itemExtent: 80,
+          itemExtent: 120,
           delegate: SliverChildBuilderDelegate(
             (BuildContext context, int index) {
-              return Container(
-                alignment: Alignment.center,
-                child: Container(
-                  decoration: BoxDecoration(
-                    border: Border(
-                      bottom: BorderSide(color: Color(0xffaeaeae), width: 0.5),
-                    ),
+              return Column(
+                children: <Widget>[
+                  Container(
+                    alignment: Alignment.centerLeft,
+                    padding: EdgeInsets.only(top: 10, bottom: 10, left: 5),
+                    child: Text('Active users'),
                   ),
-                  padding: EdgeInsets.only(bottom: 10, top: 10),
-                  child: this._buildActiveUserWidget(state),
-                ),
+                  Flexible(
+                    flex: 1,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        border: Border(
+                          bottom:
+                              BorderSide(color: Color(0xffaeaeae), width: 0.5),
+                        ),
+                      ),
+                      padding: EdgeInsets.only(bottom: 10, top: 10),
+                      child: this._buildActiveUserWidget(state),
+                    ),
+                  )
+                ],
               );
             },
             childCount: 1,
